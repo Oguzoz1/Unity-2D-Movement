4ff0990fa1d0ae12f8c4babbb85a0179974c84ee
Oguz Oz <96492946+Oguzoz1@users.noreply.github.com>
2022-03-14 12:49:09 +0300

    Create PlayerMovement.cs

---
+++ PlayerMovement.cs
@@ -0,0 +1,53 @@
+    using UnityEngine;
+    
+    [SerializeField] float speed;
+    private Rigidbody2D rb;
+    
+    private void Start()
+    {
+         rb = gameObject.GetComponent<Rigidbody2D>();
+        
+    }
+    void FixedUpdate()
+    {      
+       movement();
+    }
+		void movement()
+		{
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            var ySpeed = speed * Time.deltaTime;
+            rb.velocity = new Vector2(rb.velocity.x, ySpeed);
+
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            var ySpeed = speed * Time.deltaTime;
+            rb.velocity = new Vector2(rb.velocity.x, -ySpeed);
+
+           
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            var ySpeed = speed * Time.deltaTime;
+            rb.velocity = new Vector2(ySpeed, rb.velocity.y);
+            var rotat = - 180;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, rotat, transform.rotation.z);
+
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            var ySpeed = speed * Time.deltaTime;
+            rb.velocity = new Vector2(-ySpeed, rb.velocity.y);
+            var rotat = 0;
+            transform.rotation = Quaternion.Euler(transform.rotation.x, rotat, transform.rotation.z);
+
+		
+        }
+
+    }
+}
+    
